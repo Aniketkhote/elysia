1444d6f141cd52b13ec8f90e8bed29c35f0c3761
Aniketkhote <Aniketkhote99@gmail.com>
2025-06-29 21:56:58 +0530

    swagger added

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import { Elysia } from "elysia";
+import swagger from "@elysiajs/swagger";
 
-const app = new Elysia().get("/", () => "Hello Coolify Elysia from port 3000").listen(3000);
+const app = new Elysia()
+  .use(swagger())
+  .get("/", () => "Hello Coolify Elysia from port 3000")
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
